6646b587b82387a8482c3ba635008518dae30f52
minh <pointman449@gmail.com>
2019-07-05 10:56:50 -0700

    update test

--- tests/header.test.js
+++ tests/header.test.js
@@ -6,7 +6,7 @@ let page;
 //run before each test
 beforeEach(async () => {
     page = await Page.build();
-    await page.goto("localhost:3000");
+    await page.goto("http://localhost:3000");
 });
 
 //close browser run after test
