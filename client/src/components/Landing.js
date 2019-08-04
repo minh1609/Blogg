d934a1752cbb7dd93a39d6ddad1b3b01e40eedd6
minh <hoangminh160997@gmail.com>
2019-08-04 13:34:48 -0700

    update landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,33 @@
 import React from "react";
 
+const Icon = props => {
+    return (
+        <div className="col-sm-6 col-md-3 my-3 " style={{ color: "#1a237e" }}>
+            <i class={props.icon} style={{ fontSize: "80px" }} />
+            <h5>{props.content}</h5>
+        </div>
+    );
+};
+
 const Landing = () => {
     return (
-        <div style={{ textAlign: "center" }}>
-            <h1>Log in to write your first secret blog</h1>
-        </div>
+        <React.Fragment>
+            <div style={{ textAlign: "center" }} className="row my-5">
+                <Icon icon="fab fa-google" content="Log in with google" />
+                <Icon
+                    icon="fab fa-js-square"
+                    content="Built with React and Node"
+                />
+                <Icon
+                    icon="fas fa-user-secret"
+                    content="All user data is kept secret"
+                />
+                <Icon
+                    icon="fas fa-server"
+                    content="Optimize server performance with Redis"
+                />
+            </div>
+        </React.Fragment>
     );
 };
 
